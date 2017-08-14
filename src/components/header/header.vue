24e6c5409cb291a1b44f318822611aa6f267300e
wangqingkai <473254295@qq.com>
2017-08-14 23:31:53 +0800

    headerpop

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -33,6 +33,28 @@
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <star :size="48" :score="seller.score"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="item in seller.supports">
+              <span class="icon" :class="classMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <div class="content">{{seller.bulletin}}</div>
+          </div>
         </div>
       </div>
       <div class="detail-close">
@@ -43,6 +65,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import star from 'components/star/star';
+
   export default {
     props: {
       seller: {
@@ -58,6 +82,9 @@
       this.$nextTick(function() {
         this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       });
+    },
+    components: {
+      star: star
     }
   };
 </script>
@@ -195,6 +222,54 @@
           text-align: center
           font-size: 16px
           font-weight: 700
+        .star-wrapper
+          margin-top: 18px
+          padding: 2px 0
+          text-align: center
+        .title
+          display: flex
+          width: 80%
+          margin: 28px auto 24px auto
+          .line
+            flex: 1
+            position: relative
+            top: -6px
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+          .text
+            padding: 0 12px
+            font-size: 14px
+            font-weight: 700
+        .supports
+          width: 80%
+          margin: 0 auto
+          .support-item
+            padding: 0 12px
+            margin-bottom: 12px
+            font-size: 0
+            &:last-child
+              margin-bottom: 0
+            .icon
+              display: inline-block
+              vertical-align: top
+              width: 16px
+              height: 16px
+              margin-right: 6px
+              background-size: 16px 16px
+              background-repeat: no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              line-height: 16px
+              font-size: 12px
+
     .detail-close
       position: relative
       width: 32px
