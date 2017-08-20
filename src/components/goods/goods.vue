309d6585eb2c976e1d928b8f7c7b6b9730ea5ac7
wangqingkai <473254295@qq.com>
2017-08-20 14:05:17 +0800

    content layout

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>
-    我是goods
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  const ERR_OK = 0;
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data: function () {
+      return {
+        goods: []
+      };
+    },
+    mounted: function () {
+      this.$nextTick(function () {
+        this.getGoodsData();
+      });
+    },
+    methods: {
+      getGoodsData: function () {
+        this.$http.get('/api/goods').then((response) => {
+          let res = response.body;
+          if (res.errno === ERR_OK) {
+            this.goods = res.data;
+          }
+        });
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
+      background: #fff
 </style>
--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -21,7 +21,7 @@
     },
     computed: {
       starType: function() {
-        return 'size-' + this.size;
+        return 'star-' + this.size;
       },
       itemClasses: function() {
         let result = [];
@@ -57,7 +57,7 @@
         height: 20px
         margin-right: 22px
         background-size: 20px 20px
-        &.last-child
+        &:last-child
           margin-right: 0
         &.on
           bg-image('star48_on')
@@ -71,7 +71,7 @@
         height: 15px
         margin-right: 6px
         background-size: 15px 15px
-        &.last-child
+        &:last-child
           margin-right: 0
         &.on
           bg-image('star36_on')
@@ -85,7 +85,7 @@
         height: 10px
         margin-right: 3px
         background-size: 10px 10px
-        &.last-child
+        &:last-child
           margin-right: 0
         &.on
           bg-image('star24_on')
